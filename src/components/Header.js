3b0337db5a73db4b08d634810afb0483c16ea9eb
HannahStarcevich <starcevichh@gmail.com>
2021-06-07 08:05:44 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react"; 
+
+function Header(){
+    return (
+        <header className="container mx-auto">
+            <Heading as="hi">Records</Heading>
+            </header>
+    )
+}
+
+export default Header; 
